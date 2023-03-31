3cbf9b7f7c4b247033741bbb130fdf45d0ea3182
Waren Gador <123795328+Renstrio24p@users.noreply.github.com>
2023-03-31 18:22:08 +0800

    utilized 2 api inprogress

--- js/multiple-api.js
+++ js/multiple-api.js
@@ -1,39 +1,48 @@
-// My Prototype Syntax - Waren Gador
+// // My Prototype Syntax - Waren Gador
 
-// consist of key in 3 different api's given below
+const dictionaryApiKey = '98a198a3-a200-490a-ad48-98ac95b46d80';
+const pexelsApiKey = 'P5QhWewHGb3ek9Mmq362jR0qT6BBSlPMLBg5iKE5RinKC3BblQGafGqA';
+const openWeatherApiKey = '36b90ff89a52d49f85627b18ea50ed81';
 
-const Merriamkey = '98a198a3-a200-490a-ad48-98ac95b46d80';
-const Pexelkey = 'P5QhWewHGb3ek9Mmq362jR0qT6BBSlPMLBg5iKE5RinKC3BblQGafGqA';
-const OpenWeatherkey = '36b90ff89a52d49f85627b18ea50ed81';
+const searchButton = document.getElementById('search-button');
+searchButton.addEventListener('click', handleSearch);
 
-// Throw a search function to get data from a user input
+function handleSearch() {
+  const searchQuery = document.getElementById('search-input').value;
 
-function SearchAWord(word,city){
-    const MerriamUrl = `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${word}?key=${Merriamkey}`;
-    const PexelUrl = `https://api.pexels.com/v1/search?query=${word}&amp;per_page=1`;  
-    const OpenWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&amp;appid=${OpenWeatherkey}&amp;units=metric`;
-
-    // Fetching all response to these 3 api from given keys
-
-    Promise.all([
-        fetch(MerriamUrl),
-        fetch(PexelUrl,{headers:{ Authorization: Pexelkey}}),
-        fetch(OpenWeatherUrl)
-    
-    
-    ]).then(responses => Promise.all(responses.map(response => response.json()))
-    ).then(data => {
-        const definition = data[0][0].shortdef[0];
-      const imageUrl = data[1].photos[0].src.large;
-
-      // display the definition and image in the HTML page, we can do this one later on
-      document.getElementById('definition').textContent = definition;
-      document.getElementById('image').setAttribute('src', imageUrl);
-    })
-    .catch(error => {
-      console.log(error);
+  fetch(`https://dictionaryapi.com/api/v3/references/collegiate/json/${searchQuery}?key=${dictionaryApiKey}`)
+    .then(response => response.json())
+    .then(data => {
+      const definition = data[0].shortdef[0];
+      const partOfSpeech = data[0].fl;
+      document.getElementById('definition').innerHTML = definition;
+      document.getElementById('part-of-speech').innerHTML = partOfSpeech;
     });
-    
+
+  fetch(`https://api.pexels.com/v1/search?query=${searchQuery}`, {
+    headers: {
+      Authorization: pexelsApiKey
+    }
+  })
+    .then(response => response.json())
+    .then(data => {
+      const imageUrl = data.photos[0].src.medium;
+      document.getElementById('image').src = imageUrl;
+    });
+
+  navigator.geolocation.getCurrentPosition(position => {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&amp;lon=${longitude}&amp;appid=${openWeatherApiKey}`)
+      .then(response => response.json())
+      .then(data => {
+        const temperature = data.main.temp;
+        const weatherDescription = data.weather[0].description;
+        document.getElementById('temperature').innerHTML = temperature;
+        document.getElementById('weather-description').innerHTML = weatherDescription;
+      });
+  });
 }
 
 
@@ -55,5 +64,4 @@ Reconstruct by. Waren Gador
  Message: I will may add some designs for these 3 api with fetching elements and classes to provide.
 
 */
-
 
