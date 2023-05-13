f688687e8d83561797788c8204354a93277a5bbb
daoistcoder <deservekindness@gmail.com>
2023-05-13 15:34:23 +0800

    update NotFound.jsx for route protection

--- es-dictionary/src/views/NotFound.jsx
+++ es-dictionary/src/views/NotFound.jsx
@@ -1,8 +1,12 @@
 export default function NotFound() {
 
     return (
-        <div>
-            404 - Page Not Found
+        <div className="login-signup-form animated fadeInDown">
+            <div className="form">
+                <h1 className="title">
+                    404 - Page Not Found
+                </h1>
+            </div>
         </div>
     )
 
